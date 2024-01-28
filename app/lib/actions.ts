748f17c989656d7d588ad1414b298191c33d5dbc
Sergey <serega1997@yandex.ru>
2024-01-28 19:13:34 +0300

    chapter 14 fixed and fullCompleted

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -14,10 +14,10 @@ export type State = {
 };
 
 const FormSchema = zod.object({
-  id: zod.string({
+  id: zod.string(),
+  customerId: zod.string({
 		invalid_type_error: 'Please select a customer.',
 	}),
-  customerId: zod.string(),
   amount: zod.coerce
 		.number()
 		.gt(0, { message: 'Please enter an amount greater than $0.' }),
@@ -32,22 +32,24 @@ const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
 export async function createInvoice(prevState: State, formData: FormData) {
   const validatedFields = CreateInvoice.safeParse({
-		customerId: formData.get('customerId'),
-			amount: formData.get('amount'),
-			status: formData.get('status'),
-		})
-		if (!validatedFields.success) {
-			return {
-				errors: validatedFields.error.flatten().fieldErrors,
-				message: 'Missing Fields. Failed to Create Invoice.',
-			};
-		}
+    customerId: formData.get('customerId'),
+    amount: formData.get('amount'),
+    status: formData.get('status'),
+  });
+ 
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Missing Fields. Failed to Create Invoice.',
+    };
+  }
 	 
 		// Prepare data for insertion into the database
 		const { customerId, amount, status } = validatedFields.data;
-
-	  const amountInCents = amount * 100;
+		const amountInCents = amount * 100;
 		const date = new Date().toISOString().split('T')[0];
+	 
 		try {
 			await sql`
 				INSERT INTO invoices (customer_id, amount, status, date)
